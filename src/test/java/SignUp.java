9cfb97ab4a7468d8d7826f3b545deb5cacdb8c68
Dede Nurhidayat <ranmaniax@gmail.com>
2023-10-19 22:40:16 +0700

    login and sign up

--- src/test/java/SignUp.java
+++ src/test/java/SignUp.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class Login {
+public class SignUp {
     @Test //Tag untuk running script  di bawah ini
     public void open_browser(){
 
@@ -19,11 +19,16 @@ public class Login {
         String title = driver.getTitle();
         System.out.println(title);
 
+        String klik_daftar = "ingin mencoba, daftar ?";
+        driver.findElement(By.partialLinkText(klik_daftar)).click();
+
+        driver.findElement(By.id("name")).sendKeys("Toko Senjata Feria Shen");
         driver.findElement(By.id("email")).sendKeys("feriashen@naraka.com");
         driver.findElement(By.id("password")).sendKeys("feriashen");
         driver.findElement(By.xpath("//button[@type='submit']")).click();
 
         driver.close();
+
 
     }
 }
